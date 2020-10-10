d048edcb66051f0c6b45f894abe9ac256f64715b
roopeh04 <arvapalli12345@gmail.com>
2020-10-11 00:48:13 +0530

    Initial Commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 
-url=process.env.URL
+url=process.env.LOCAL_URL
 mongoose.connect(url,{
     useNewUrlParser:true,
     useCreateIndex:true,
--- src/index.js
+++ src/index.js
@@ -13,6 +13,11 @@ app.use(bodyPraser.urlencoded({extended:true}))
 app.use(express.json())
 app.use(dataRouter)
 app.use(uploadRouter)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/',function(req,res){
     res.send("It is working")
